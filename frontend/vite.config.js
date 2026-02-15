7b240645ddb1531ab8d0d570227105af58a94772
David Jiang <jiangguanlin2003@outlook.com>
2026-02-15 18:25:18 +0800

    fix: frontend proxy bug

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,9 +6,9 @@ const getBackendUrl = () => {
   if (process.env.BACKEND_URL) {
     return process.env.BACKEND_URL
   }
-  // Default to localhost for local development
-  // In Docker, BACKEND_URL should be set to http://host.docker.internal:8181
-  return 'http://localhost:8181'
+  // Use 127.0.0.1 (IPv4) so proxy doesn't try ::1 (IPv6) and get ECONNREFUSED on some servers
+  // In Docker, set BACKEND_URL=http://host.docker.internal:8181
+  return 'http://127.0.0.1:8181'
 }
 
 // https://vite.dev/config/
